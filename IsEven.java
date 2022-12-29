5e2c90ab7df7a28bf0727f84f838ef9adc3225ea
IrinkaCh <123irina00@mail.ru>
2022-12-29 15:02:26 +0500

    IsEven

---
+++ IsEven.java
@@ -0,0 +1,14 @@
+//IsEven — ему дают целое число, он одобряет его, если оно чётное
+
+public class IsEven<T> implements IsGood<T> {
+
+    @Override
+    public boolean isGood(T number) {
+        if (number instanceof Integer) {
+            if ((Integer) number % 2 == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
